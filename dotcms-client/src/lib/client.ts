f46aac2765dca65daa749d8a7e7d084ad21c97f8
Jalinson Diaz <zjaaaldev@gmail.com>
2025-03-10 17:39:42 -0300

    add configuration usage to the client

--- dotcms-client/src/lib/client.ts
+++ dotcms-client/src/lib/client.ts
@@ -1,7 +1,15 @@
-import { PageAPIParams } from './types';
+import { DotCMSConfig, PageAPIParams } from './types';
 
 export class DotCMSClient {
-    constructor() {}
+    #token: string;
+    #siteID: string;
+    #baseURL: string;
+
+    constructor({ baseURL, siteID, token }: DotCMSConfig) {
+        this.#baseURL = baseURL;
+        this.#siteID = siteID;
+        this.#token = token;
+    }
 
     async getPage({ path, languageID }: PageAPIParams) {}
 }
