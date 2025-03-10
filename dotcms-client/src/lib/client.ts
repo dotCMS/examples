dcf0cd68d9b90958c6c596b13e05234e25664c68
Jalinson Diaz <zjaaaldev@gmail.com>
2025-03-10 17:40:42 -0300

    add fetch handler

--- dotcms-client/src/lib/client.ts
+++ dotcms-client/src/lib/client.ts
@@ -13,5 +13,25 @@ export class DotCMSClient {
         this.#token = token;
     }
 
-    async getPage({ path, languageID }: PageAPIParams) {}
+    async getPage({ path, languageID }: PageAPIParams) {
+        const queryParams = new URLSearchParams();
+        queryParams.set('host_id', this.#siteID);
+        queryParams.set('languageId', languageID ?? '1');
+
+        const url = new URL(`${PAGE_API_PATH}${path}?${queryParams.toString()}`, this.#baseURL);
+
+        const response = await fetch(url, {
+            headers: {
+                Authorization: `Bearer ${this.#token}`
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch page: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+
+        return data;
+    }
 }
