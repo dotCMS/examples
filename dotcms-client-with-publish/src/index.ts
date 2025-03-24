7809bb33631e016e96f599bcf988e71f8a565023
Jalinson Diaz <zjaaaldev@gmail.com>
2025-03-24 17:48:50 -0300

    prepare index for sdk exposition

--- dotcms-client-with-publish/src/index.ts
+++ dotcms-client-with-publish/src/index.ts
@@ -1,16 +1,2 @@
-import { DotCMSClient } from './lib/client';
-
-const client = new DotCMSClient({
-    baseURL: '<your-base-url>',
-    token: '<your-token>',
-    siteID: '<your-site-id>'
-});
-
-(async () => {
-    const page = await client.getPage({
-        path: '/',
-        languageID: '1'
-    });
-
-    console.log(page);
-})();
+export { DotCMSClient } from './lib/client';
+export type { DotCMSConfig, PageAPIParams } from './lib/types';
